cc8aa39cc8d3e6afee1c12bf1628edb0eca85fda
Dawid <rygula.dawid@gmail.com>
2025-06-12 09:47:09 +0200

    [v5] Add offer filters

---
+++ src/venari/offer_filters.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+from venari.models import JobOffer
+
+
+class FilterInterface(ABC):
+    @staticmethod
+    @abstractmethod
+    def filter_offer(offer: JobOffer) -> bool:
+        return True
+
+
+class IgnoreAIOffers(FilterInterface):
+    @staticmethod
+    def filter_offer(offer: JobOffer) -> bool:
+        title = offer.title.lower()
+
+        if "ai" in title or "machine learning" in title:
+            return False
+
+        return True
+
+
+class MinimumWage100(FilterInterface):
+    @staticmethod
+    def filter_offer(offer: JobOffer) -> bool:
+        if offer.salary is None or offer.salary.min < 100:
+            return False
+
+        return True
